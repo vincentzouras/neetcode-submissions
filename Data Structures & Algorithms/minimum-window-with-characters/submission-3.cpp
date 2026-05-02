a044173be148f6731ec6a27cd216ade113f6bd15
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-05-02 15:26:18 -0400

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size(), m = t.size();
+        if (m > n) return "";
+
+        vector<int> need(128);
+        int required = 0;
+        for (char c : t) {
+            need[c]++;
+            if (need[c] == 1) required++; // increment for distinct
+        }
+        
+        vector<int> have(128);
+        int formed = 0;
+
+        string shortest_substring = "";
+        int l = 0;
+        int r = 0;
+        for (; r < n; r++) {
+            // add to window
+            have[s[r]]++; 
+
+            // update formed if we just got enough of it
+            if (have[s[r]] == need[s[r]]) formed++;
+
+            // try shrinking l while we have valid window
+            while (formed == required) {
+                // update answer
+                if ((r - l + 1) < shortest_substring.size() || shortest_substring.empty()) {
+                    shortest_substring = s.substr(l, (r - l + 1));
+                }
+
+                // check if removing l from window breaks requirement
+                char removed = s[l];
+                have[s[l++]]--;
+                if (have[removed] < need[removed]) formed--;
+            }
+        }
+
+        return shortest_substring;
+    }
+};
