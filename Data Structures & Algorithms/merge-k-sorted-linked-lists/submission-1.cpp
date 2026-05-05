ca30e73d4fa7ce3f2af768197857b39b7a020367
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-05-04 22:53:47 -0400

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+private: 
+    struct Comparator {
+        bool operator()(ListNode* a, ListNode* b) { 
+            return a->val > b->val; // lower priority if true, so a is lower, therefore min
+        }
+    };
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Comparator> min_heap; 
+
+        // Add head of each list to min heap
+        for (auto head : lists) {
+            if (head) min_heap.push(head);
+        }
+
+        ListNode dummy;
+        ListNode* result = &dummy;
+
+        while (!min_heap.empty()) {
+            ListNode* smallest = min_heap.top();
+            min_heap.pop();
+
+            result->next = smallest;
+            result = result->next;
+
+            // If that list had more elements, push next onto heap
+            if (smallest->next) {
+                min_heap.push(smallest->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
