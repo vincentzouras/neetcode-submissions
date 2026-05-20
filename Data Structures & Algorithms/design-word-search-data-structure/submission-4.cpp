fca1e521eb3d5c06a091d7941846e0aff1c8fc9f
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-05-19 22:35:01 -0400

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.cpp
@@ -0,0 +1,55 @@
+class WordDictionary {
+private:
+    struct Node {
+        bool complete = false;
+        Node* children[26] = {};
+    };
+    Node* root;
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* curr = root;
+        for (char c : word) { 
+            if (!curr->children[c - 'a']) {
+                Node* node = new Node();
+                curr->children[c - 'a'] = node;
+            }
+
+            curr = curr->children[c - 'a'];
+        }
+        curr->complete = true;
+    }
+
+    bool search(const string& word, int i, Node* curr) {
+        for (; i < word.size(); i++) {
+            char c = word[i];
+
+            if (c == '.') {
+                bool match = false;
+                for (int j = 0; j < 26; j++) {
+                    if (curr->children[j] && search(word, i + 1, curr->children[j])) {
+                        match = true;
+                        break;
+                    }
+                }
+
+                return match;
+            }
+
+            if (!curr->children[c - 'a']) {
+                return false;
+            }
+
+            curr = curr->children[c - 'a'];
+        }
+
+        return curr->complete;
+    }
+    
+    bool search(string word) {
+        return search(word, 0, root);
+    }
+};
