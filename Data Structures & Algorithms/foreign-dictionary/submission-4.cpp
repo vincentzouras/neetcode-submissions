ab59c18b56bc561b84a4f7b04add9e1545f3effd
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-06-15 22:25:48 -0400

    Add: foreign-dictionary - submission-4

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-4.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string foreignDictionary(vector<string>& words) {
+        int char_count = 0;
+
+        // track indegrees, init to 0 for each letter present        
+        vector<int> indegree(26, -1);
+        for (string word : words) {
+            for (char c : word) {
+                if (indegree[c - 'a'] != 0) char_count++;
+                indegree[c - 'a'] = 0;
+            }
+        }
+
+        // build adjacency list  
+        vector<vector<int>> adj(26);
+        for (int i = 0; i < words.size() - 1; i++) { 
+            string word = words[i];
+            string next_word = words[i+1];
+            bool different = false;
+            int length = min(next_word.size(), word.size());
+
+            for (int j = 0; j < length; j++) {
+                if (next_word[j] != word[j]) {
+                    different = true;
+                    adj[word[j] - 'a'].push_back(next_word[j] - 'a');
+                    indegree[next_word[j] - 'a']++;
+                    break;
+                }
+            }
+
+            if (!different && next_word.size() < word.size()) return "";
+        }
+
+        // topological sort
+        queue<int> q;
+        string result;
+
+        for (int i = 0; i < 26; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            result.push_back(node + 'a');
+
+            for (int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+
+        if (char_count != result.size()) return "";
+
+        return result;
+    }
+};
