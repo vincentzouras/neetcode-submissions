a99f8e92023c4d2d254c5b1c34d4c236f6b1a65c
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-06-20 17:30:13 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+
+        vector<int> dp(nums.size()); // dp[i] is max money you can rob up to index i 
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[nums.size() - 1];
+    }
+};
