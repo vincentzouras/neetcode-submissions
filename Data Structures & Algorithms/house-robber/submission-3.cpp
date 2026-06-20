327c29c0a5462cb19efcffe52756296a62753177
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-06-20 17:36:23 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+
+        int curr = max(nums[0], nums[1]);
+        int prevPrev = nums[0];
+        int prev = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            curr = max(prev, nums[i] + prevPrev);
+            prevPrev = prev;
+            prev = curr;
+        }
+
+        return curr;
+    }
+};
