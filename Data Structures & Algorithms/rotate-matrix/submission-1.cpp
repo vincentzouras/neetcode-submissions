dcae9ff80c679e103552c50d2c9efa7960aff630
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-06-24 23:45:19 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        // swap val at [i][j] with [j][i], but not on whole matrix or they swap back
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse each list 
+        for (vector<int>& vec : matrix) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = vec[left];
+                vec[left] = vec[right];
+                vec[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+};
