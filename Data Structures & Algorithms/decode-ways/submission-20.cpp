30e1da861d13a683129b6036f59f1e7412b46d3f
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-06-28 23:24:43 -0400

    Add: decode-ways - submission-20

---
+++ Data Structures & Algorithms/decode-ways/submission-20.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1); // dp[i] = # ways to decode s[i:]
+        dp[n] = 1;
+
+        for (int i = n-1; i >= 0; i--) { 
+
+            if (s[i] == '0') continue;
+
+            // take one digit
+            dp[i] += dp[i+1];
+
+            // take two digits
+            if (i < n-1) {
+                int num = ((s[i]-'0') * 10) + (s[i+1]-'0');
+                if (num <= 26) dp[i] += dp[i+2];
+            }
+        }
+
+        return dp[0];
+    }
+};
