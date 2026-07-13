568b944365b718305e5da670584e2bcb402e085b
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-07-13 13:05:21 -0400

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>> &memo, int m, int n) {
+        if (m < 0 || n < 0) return 0;
+        if (m == 0 && n == 0) return 1;
+        if (memo[m][n] != 0) return memo[m][n];
+
+        int go_up = dfs(memo, m-1, n);
+        int go_left = dfs(memo, m, n-1);
+
+        memo[m][n] = go_up + go_left;
+
+        return go_up + go_left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> memo(m, vector<int>(n, 0)); 
+        // memo[i][j] = # unique paths from i,j which reach 0,0
+
+        return dfs(memo, m-1, n-1);
+    }
+
+    /*
+    Time complexity: O(m * n)
+    Space complexity: O(m * n)
+    */
+};
