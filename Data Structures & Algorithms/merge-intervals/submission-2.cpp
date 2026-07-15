fb79ae677228d7c28988988764fdf3b3a1eff19f
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-07-14 22:40:58 -0400

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> result;
+
+        sort(intervals.begin(), intervals.end(), 
+            [](const vector<int> &a, const vector<int> &b) {
+                return a[0] < b[0];
+            });
+        
+        result.push_back(intervals[0]);
+        
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] <= result.back()[1]) {
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+
+        return result;
+    }
+};
