c8e13d5c547653e981b64a63c13bb9ee85d3d734
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-09-11 11:55:39 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> clones;
+
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        return dfs(node);
+    }
+
+    Node* dfs(Node* root) {
+        if (clones.contains(root)) return clones[root];
+
+        Node* node = new Node(root->val);
+
+        clones[root] = node;
+
+        for (Node* nei : root->neighbors) {
+            Node* neighbor = dfs(nei);
+            node->neighbors.push_back(neighbor);
+        }
+
+        return node;
+    }
+};
