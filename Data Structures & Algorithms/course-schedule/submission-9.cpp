87244d4e3f93b7349f415e249b0cdd0e2ed7e4fb
vincentzouras <108770151+vincentzouras@users.noreply.github.com>
2026-09-14 13:59:31 -0400

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> inorder(numCourses);
+
+        for (vector<int> prereq : prerequisites) {
+            graph[prereq[1]].push_back(prereq[0]);
+            inorder[prereq[0]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (inorder[i] == 0) q.push(i);
+        }
+
+        int count = 0;
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            count++;
+
+            for (int unlocked : graph[course]) {
+                inorder[unlocked]--;
+                if (inorder[unlocked] == 0) q.push(unlocked);
+            }
+        }
+
+        return count == numCourses;
+    }
+};
